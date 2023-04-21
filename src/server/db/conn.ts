28d12026361fe5dd3d6964d63f437e0248bd3e7a
Naveen M K <naveen521kk@gmail.com>
2023-04-22 00:38:21 +0530

    Update fallback connection URL

--- src/server/db/conn.ts
+++ src/server/db/conn.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db as MongoDb } from "mongodb";
 
-const uri = process.env.MONGODB_URL || "mongodb://localhost:27017/";
+const uri = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
 
 export const getDb = () => {
