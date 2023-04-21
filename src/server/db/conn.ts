013a1d843ee34777b1444f8dfec6aa0f1a327f2f
Naveen M K <naveen521kk@gmail.com>
2023-04-22 00:04:07 +0530

    Use correct `uri` for mongodb connection

--- src/server/db/conn.ts
+++ src/server/db/conn.ts
@@ -1,7 +1,7 @@
 import { MongoClient, Db as MongoDb } from "mongodb";
 
 const uri = process.env.MONGODB_URL || "mongodb://localhost:27017/";
-const client = new MongoClient("mongodb://127.0.0.1:27017/");
+const client = new MongoClient(uri);
 
 export const getDb = () => {
     return client.db("books");
